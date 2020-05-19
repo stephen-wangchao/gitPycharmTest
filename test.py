af144393484d6b49bab88650fad122a0d517b34d
stephen-wangchao <ASDasd123>
2020-05-18 22:38:07 -0400

    added while condition

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ for i in range(a):
     print('x'*(i+1))
 print('updated prior code')
 print('updated directly on git again!')
+run = True
+while a > 3 and run:
+    print('input is greater than 3')
+    run = False
